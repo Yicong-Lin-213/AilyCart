458b755ba36745694476ce224b1380a6808fe8f7
Yicong Lin <yicong.lin.213@gmail.com>
2026-02-25 13:50:51 -0800

    Added edit feature in Result page

--- AilyCartApp/app/results.tsx
+++ AilyCartApp/app/results.tsx
@@ -1,15 +1,15 @@
-import { View, Text, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, ActivityIndicator, TextInput, Platform } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import tw from '../lib/tailwind';
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabase-client';
-import { decode } from 'base64-arraybuffer';
-import * as FileSystem from 'expo-file-system/legacy';
+import { File } from 'expo-file-system';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
 
 interface ReceiptData {
     merchant: { name: string | null; address: string | null; phone: string | null };
     transaction: { date: string; time: string; receipt_number: string | null };
-    items: [{ name: string; quantity: number; price_per_unit: number; total_price: number }];
+    items: { name: string; quantity: number; price_per_unit: number; total_price: number }[];
     totals: { subtotal: number; tax: number; total: number; currency: string };
     payment_method: string;
 }
@@ -20,11 +20,17 @@ export default function Results() {
     const [ loading, setLoading] = useState(true);
     const [ receiptData, setReceiptData ] = useState<ReceiptData | null>(null);
     const [ uploadingStatus, setUploadingStatus] = useState("");
-    const [scannedItems, setScannedItems] = useState({"items":[
-        { name: "Lettuce", price: "3.00" },
-        { name: "Milk", price: "5.00" },
-        { name: "Eggs", price: "10.00" },
-    ]});
+    const [originalItemNames, setOriginalItemNames] = useState<string[]>([]);
+    const [nameMapping, setNameMapping] = useState<{ [key: string]: string }>({ });
+    const [showDataPicker, setShowDataPicker] = useState(false);
+
+    const getDateObject = () => {
+        if (receiptData?.transaction?.date) {
+            const d = new Date(receiptData?.transaction.date);
+            return isNaN(d.getTime()) ? new Date() : d;
+        }
+        return new Date();
+    }
 
     const processImages = async () => {
         console.log("Processing images:", images);
@@ -36,11 +42,12 @@ export default function Results() {
 
             const uploadToSupabase = async (uri: string, index: number) => {
                 const fileName = `receipt_${Date.now()}_${index}.jpg`;
-                const base64 = await FileSystem.readAsStringAsync(uri, {encoding: "base64" });
+                const file = new File(uri);
+                const arrayBuffer = await file.arrayBuffer();
 
                 const { data, error } = await supabase.storage
                     .from('receipt_images')
-                    .upload(fileName, decode(base64), { contentType: 'image/jpeg' });
+                    .upload(fileName, arrayBuffer, { contentType: 'image/jpeg', upsert: true });
 
                 if (error) throw error;
 
@@ -61,11 +68,47 @@ export default function Results() {
 
             const data = await response.json();
             setReceiptData(data.payload);
+
+            setOriginalItemNames(data.items?.map((item: any) => item.name) || []);
         } catch (error) {
             console.error("Processing failed", error);
         } finally {
             setLoading(false);
         }
+    };
+
+    const handleNameChange = (newName: string, index: number) => {
+        if (!receiptData) return;
+
+        const originalName = originalItemNames[index];
+        const newItems = [...receiptData.items];
+        newItems[index].name = newName;
+        setReceiptData({ ...receiptData, items: newItems});
+
+        if (newName !== originalName) {
+            setNameMapping(prev => ({ ...prev, [originalName]: newName }));
+        }
+    };
+
+    const handlePriceChange = (newPrice: string, index: number) => {
+        if (!receiptData) return;
+        const newItems = [...receiptData.items];
+        newItems[index].total_price = parseFloat(newPrice) || 0;
+        setReceiptData({ ...receiptData, items: newItems});
+    };
+
+    const onDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+        if (event.type === 'dismissed' ) {
+            setShowDataPicker(false);
+            return;
+        }
+
+        if (selectedDate) {
+            if (Platform.OS === 'android') setShowDataPicker(false);
+        }
+
+        const formattedDate = selectedDate?.toISOString().split('T')[0] || "";
+        setReceiptData(prev => prev ? { ...prev, transaction: { ...prev.transaction, date: formattedDate }} : null);
     };
 
     useEffect(() => {
@@ -84,10 +127,40 @@ export default function Results() {
     }
 
     return (
-        <View style={tw`flex-1 bg-aily-bg p-8 pt-16`}>
+        <View style={tw`flex-1 bg-aily-bg p-8 pt-22`}>
             <View style={tw`mb-6`}>
-                <Text style={tw`text-aily-h2 font-atkinson-bold text-aily-primary`}>{receiptData?.merchant?.name || "Results"}</Text>
-                <Text style={tw`text-aily-body-sm font-atkinson text-aily-secondary`}>Date: {receiptData?.transaction?.date || "N/A"}</Text>
+                <TextInput style={tw`text-aily-h2 font-atkinson-bold text-aily-primary`}
+                    value={receiptData?.merchant?.name || ""}
+                    onChangeText={(text) => setReceiptData(prev => prev ? { ...prev, merchant: { ...prev?.merchant, name: text }} : null)}
+                />
+                <View style={tw`mb-6`}>
+                    <TouchableOpacity 
+                        onPress={() => setShowDataPicker(true)}
+                        style={tw`bg-white pt-4 flex-row justify-between items-center`}
+                    >
+                        <Text style={tw`text-aily-body-sm font-atkinson text-aily-secondary`}>
+                            {receiptData?.transaction?.date || "Select Date"}
+                        </Text>
+                    </TouchableOpacity>
+                    {showDataPicker && (
+                        <DateTimePicker
+                            value={getDateObject()}
+                            mode='date'
+                            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                            onChange={onDateChange}
+                            maximumDate={new Date()}
+                        />
+                    )}
+                    
+                    {showDataPicker && Platform.OS === 'ios' && (
+                        <TouchableOpacity
+                            onPress={ ()=>setShowDataPicker(false) }
+                            style={tw`mt-2 times-end`}
+                        >
+                            <Text style={tw`text-aily-blue font-atkinson-bold p-2`}>Done</Text>
+                        </TouchableOpacity>
+                    )}
+                </View>
             </View>
             
             {/* Goods */}
@@ -95,10 +168,20 @@ export default function Results() {
                 {receiptData?.items && receiptData?.items.length > 0 ? ( receiptData.items.map((item, index) => (
                     <View key={index} style={tw`flex-row justify-between py-4 border-b border-gray-100`}>
                         <View style={tw`flex-1`}>
-                            <Text style={tw`text-aily-body-lg font-atkinson text-aily-primary`}>{item.name}</Text>
+                            <TextInput 
+                                style={tw`text-aily-body-lg font-atkinson text-aily-primary`}
+                                value={item.name}
+                                onChangeText={(text) => handleNameChange(text, index)}
+                                placeholder="Item Name"
+                            />
                             <Text style={tw`text-aily-body-sm font-atkinson-bold text-aily-primary`}>Qty: {item.quantity} x {item.price_per_unit}</Text>
                         </View>
-                        <Text style={tw`text-aily-body-lg font-atkinson-bold text-aily-primary`}>${item.total_price.toFixed(2)}</Text>
+                        <TextInput
+                            style={tw`text-aily-body-lg font-atkinson-bold text-aily-primary`}
+                            value={item.total_price.toFixed(2)}
+                            onChangeText={(text) => handlePriceChange(text, index)}
+                            keyboardType='numeric'
+                        />
                     </View>
                 ))) : (
                     <View style={tw`h-full flex items-center justify-center`}>
